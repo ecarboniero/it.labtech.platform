0cd8ddad45ffb8289d5ad79a86bdce1c3a4822b6
ecarboniero <enrico.carboniero@gmail.com>
2020-12-29 13:42:25 +0000

    Add api module with starter class.

---
+++ api/src/main/java/it/labtech/platform/api/Starter.java
@@ -0,0 +1,13 @@
+package it.labtech.platform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+}
